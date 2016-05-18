ff4a48545bae80a7c51bff9ceef27d319801f74b
Aaron Schlesinger <arschles@gmail.com>
2016-05-18 10:12:25 -0700

    fix(config.go): allow env vars to be shorter names (#50)

--- config/config.go
+++ config/config.go
@@ -4,13 +4,13 @@ import "github.com/kelseyhightower/envconfig"
 
 // Specification config struct
 type Specification struct {
-	Port           string `default:"8080"`
-	Polling        int    `default:"43200"` // 43200 seconds = 12 hours
-	VersionsAPIURL string
-	DoctorAPIURL   string
-	APIVersion     string
-	CheckVersions  bool   `default:"true"`
-	DeisNamespace  string `default:"deis"`
+	Port           string `default:"8080" envconfig:"PORT"`
+	Polling        int    `default:"43200" envconfig:"POLL_INTERVAL_SEC"` // 43200 seconds = 12 hours
+	VersionsAPIURL string `envconfig:"VERSIONS_API_URL"`
+	DoctorAPIURL   string `envconfig:"DOCTOR_API_URL"`
+	APIVersion     string `envconfig:"API_VERSION"`
+	CheckVersions  bool   `default:"true" envconfig:"CHECK_VERSIONS"`
+	DeisNamespace  string `default:"deis" envconfig:"DEIS_NAMESPACE"`
 }
 
 // Spec is an exportable variable that contains workflow manager config data
